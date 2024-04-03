522b651e739d70f2d4243a47665451ec1f884991
Martin SEBIROT <martinzs1@yahoo.fr>
2024-04-03 12:30:50 +0200

    show inbox messages in index pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
+import fs from "fs"
+import path from "path"
 import Link from "next/link"
 
+function getFilesInDirectory(directory: string): string[] {
+  let results: string[] = [];
+
+  const files = fs.readdirSync(directory);
+
+  for (const file of files) {
+      const filePath = path.join(directory, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat && stat.isDirectory()) {
+          results = results.concat(getFilesInDirectory(filePath));
+      } else {
+          results.push(filePath);
+      }
+  }
+
+  return results;
+}
+
 export default async function Home() {
+  const files = getFilesInDirectory("src/app/inbox");
   return(
     <div>
        <Link href="/view/01">working message</Link><br />
-      <Link href="/view/02">broken message</Link><br />
+       <Link href="/view/02">broken message</Link><br />
+        {
+          files.map((file, index) => {
+            //const data = path.parse(file);
+            return (
+              <div key={index}>
+                <Link href={`/view/${path.parse(file).name}`}>{path.parse(file).name}</Link>
+              </div>
+            );
+          })
+        }
     </div>
   );
 }
