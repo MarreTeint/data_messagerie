eb966f420b6f25626dca258d0769e94b5f1f774b
Martin SEBIROT <martinzs1@yahoo.fr>
2024-04-03 11:54:31 +0200

    updated schema

--- src/app/view/[slug]/page.tsx
+++ src/app/view/[slug]/page.tsx
@@ -2,10 +2,14 @@ import Message from './Message';
 import SideBar from './SideBar';
 
 export default function Page({ params }: { params: { slug: string } }) {
-  return(
-  <div>
-    <Message params={params} />
-    <SideBar />
-  </div>
+  return (
+    <div className="flex">
+      <div className="w-1/4 border-r border-gray-300">
+        <SideBar />
+      </div>
+      <div className="w-3/4">
+        <Message params={params} />
+      </div>
+    </div>
   );
 }
