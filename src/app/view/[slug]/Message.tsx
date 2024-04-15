1498195e5d32e770cbeafcd66283909ac1a553a3
MarreTeint <martinzs1@yahoo.fr>
2024-04-15 16:32:03 +0200

    add core extentions nedd to add verification of who send it & external extentions

--- src/app/view/[slug]/Message.tsx
+++ src/app/view/[slug]/Message.tsx
@@ -12,17 +12,22 @@ function ErrorMessage(){
     );
 }
 
-function CoreExtentions(coreExtentions: any){   //readed: boolean, yes_no: {question: string, asnwer: boolean|null}, multiple_choice: {question: string, option: string[], answer: number}, time_selector: {hour: number, minutes: number}, color_selector: {red: number, green: number, blue: number}
-    return(
-        <div></div>
-    );
-}
-
 export default function Message({ params }: { params: { slug: string } }) {
     const router = useRouter();
-    const [data, setData] = useState({ metadata: {title:"", sender:"", date:""}, message: "", coreExtentions:{}});
+    const [data, setData] = useState({ 
+        metadata: {title:"", sender:"", date:""}, 
+        message: "", 
+        core_extentions:{
+            "readed": false,
+            "yes_no": {"question": "", "answer": false}, 
+            "multiple_choice": {"question": "Which option do you prefer for lunch?","options": ["Sandwiches", "Salad", "Pizza"],"answer": "Sandwiches"},
+            "time_selector": {"hour": 9, "minutes": 30},
+            "color_selector": {"red": 128,"green": 0,"blue": 255}
+        }
+    });
     const [valid, setValid] = useState({valid: false, errors: []});
 
+    // Verify that the message exists & put it in data
     useEffect(() => {
         try {
             const din = require(`./../../inbox/${params.slug}.json`);
@@ -33,6 +38,7 @@ export default function Message({ params }: { params: { slug: string } }) {
     }, [params.slug, router]);
     const schema = require('./../../schema.json');
     registerSchema(schema, "http://example.com/schema.json");
+    // Validate message with schema
     useEffect(() => {
         validate("http://example.com/schema.json", data).then((result:any) => {
             setValid(result);
@@ -46,19 +52,60 @@ export default function Message({ params }: { params: { slug: string } }) {
         return <ErrorMessage/>;
     }
 
+    const date = new Date(data.metadata.date);
+
     return (
-      <div>
-        sender: {data.metadata.sender}
-        <br />
-        title: {data.metadata.title}
-        <br />
-        date: {data.metadata.date}
-        <br />
-        message: {data.message}
-        <br />
-        <CoreExtentions/>
-        {JSON.stringify(valid)}
-      </div>
+        <div>
+            <div>
+                from: {data.metadata.sender}
+                <br />
+                title: {data.metadata.title} - {date.toString()}
+                <br /><hr />
+                {data.message}
+                <br /><hr />
+            </div>
+
+            {data.core_extentions.readed !== undefined && (
+                <div>
+                    <label>Message readed: </label>
+                    <input type="checkbox" checked={data.core_extentions.readed} />
+                </div>
+            )}
+            {data.core_extentions.yes_no !== undefined && (
+                <div>
+                    <label>{data.core_extentions.yes_no.question}</label>
+                    <br />
+                    <input type="radio" name="answeryn" value="yes" checked={data.core_extentions.yes_no.answer} />
+                    <label>Yes</label>
+                    <input type="radio" name="answeryn" value="no" checked={!data.core_extentions.yes_no.answer && data.core_extentions.yes_no.answer != null} />
+                    <label>No</label>
+                </div>
+            )}
+            {data.core_extentions.multiple_choice !== undefined && (
+                <div>
+                    <label>{data.core_extentions.multiple_choice.question}</label>
+                    <br />
+                    {data.core_extentions.multiple_choice.options.map((option: string) => (
+                        <div key={option}>
+                            <input type="radio" name="answer" value={option} checked={data.core_extentions.multiple_choice.answer === option} />
+                            <label>{option}</label>
+                        </div>
+                    ))}
+                </div>
+            )}
+            {data.core_extentions.time_selector !== undefined && (
+                <div>
+                    <label>Time: </label>
+                    <input type="time" value={String(data.core_extentions.time_selector.hour).padStart(2, '0')+':'+String(data.core_extentions.time_selector.minutes).padStart(2, '0')} />
+                </div>
+            )}
+            {data.core_extentions.color_selector !== undefined && (
+                <div>
+                    <label>Color: </label>
+                    <input type="color" value={'#'+data.core_extentions.color_selector.red.toString(16).padStart(2, '0')+data.core_extentions.color_selector.green.toString(16).padStart(2, '0')+data.core_extentions.color_selector.blue.toString(16).padStart(2, '0')}/>
+                </div>
+            )}
+        </div>
     );
 }
     
