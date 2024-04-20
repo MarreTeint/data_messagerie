ef78b0819a709bd98482cbfbd4f7c2bf3804c59a
MarreTeint <martinzs1@yahoo.fr>
2024-04-20 16:17:53 +0200

    link to return home on view of message

--- src/app/view/[slug]/TopBar.tsx
+++ src/app/view/[slug]/TopBar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function TopBar({ params }: { params: { slug: string } }) {
     return (
         <div>
+            <Link href="/"><button>Return Home</button></Link>
             <Link href={`./../rep/${params.slug}`}><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Answer</button></Link>
         </div>
     );
